4452e68bd8623af98b84bfbd55d3a2180e32b1f2
Hely Mendez <skhely998@gmail.com>
2020-06-11 20:47:57 -0600

    added routes api and model

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const connection = require('./models');
 const express = require('express');
 const app = express();
+
+// import routes
+const songRoute = require('./controllers/song');
+app.use('/song', songRoute);
 
 //Routes
 app.get('/', (req, res)=>{
