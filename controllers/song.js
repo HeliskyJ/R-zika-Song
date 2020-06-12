1edb0647c1a127816dfddac0aab358116f54b492
Hely Mendez <skhely998@gmail.com>
2020-06-11 21:42:50 -0600

    added update song method

--- controllers/song.js
+++ controllers/song.js
@@ -44,4 +44,24 @@ router.get('/:songId', async(req, res) => {
     }
 });
 
+//update song
+router.patch('/:songId', async (req, res) => {
+    try{
+        const updateSong = await Song.findByIdAndUpdate(req.params.songId, {
+            title : req.body.title,
+            gender : req.body.gender,
+            duration : req.body.duration,
+            artists : req.body.artists,
+            release : req.body.release,
+            position : req.body.position,
+            rankingcountry : req.body.rankingcountry,
+            lenguage : req.body.lenguage,
+            frontcover : req.body.frontcover,
+            fav : req.body.fav
+        });
+        res.json(updateSong);
+    }catch(err){
+        res.json({message : err});
+    }
+});
 module.exports = router;
